fcbb78c1df32dab4e985b85309129daf63c3366d
Mauricio Builes <rmbuilesm@unal.edu.co>
2024-11-22 17:18:05 -0500

    finailed1

--- homework/word_count.py
+++ homework/word_count.py
@@ -58,6 +58,30 @@ def count_words(dataframe):
     dataframe = dataframe.groupby("line").size().reset_index(name="count")
     return dataframe
 
+def save_output(dataframe, output_directory):
+    """Save output to a file."""
+
+    if os.path.exists(output_directory):
+        files = glob.glob(f"{output_directory}/*")
+        for file in files:
+            os.remove(file)
+        os.rmdir(output_directory)
+
+    os.makedirs(output_directory)
+
+    dataframe.to_csv(
+        f"{output_directory}/part-00000",
+        sep="\t",
+        index=False,
+        header=False,
+    )
+
+
+def create_marker(output_directory):
+    """Create Marker"""
+
+    with open(f"{output_directory}/_SUCCESS", "w", encoding="utf-8") as f:
+        f.write("")
 
 #
 # Escriba la función job, la cual orquesta las funciones anteriores.
@@ -68,7 +92,9 @@ def run_job(input_directory, output_directory):
     dataframe = load_input(input_directory)
     dataframe = clean_text(dataframe)
     dataframe = count_words(dataframe)
-    print(dataframe)
+    save_output(dataframe, output_directory)
+    create_marker(output_directory)
+
 
 
 if __name__ == "__main__":
@@ -77,3 +103,5 @@ if __name__ == "__main__":
         "files/input",
         "files/output",
     )
+
+    
